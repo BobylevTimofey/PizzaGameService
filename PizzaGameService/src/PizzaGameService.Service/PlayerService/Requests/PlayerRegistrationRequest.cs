988bb6b4e90527faa3b08cb3f41ec978245faaf6
Тимофей <bobylev_tv@taximaxim.ru>
2023-11-27 23:04:37 +0500

    Добавил валидацию

--- PizzaGameService/src/PizzaGameService.Service/PlayerService/Requests/PlayerRegistrationRequest.cs
+++ PizzaGameService/src/PizzaGameService.Service/PlayerService/Requests/PlayerRegistrationRequest.cs
@@ -1,17 +1,31 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 using PizzaGameService.Data.PlayerData.Models;
 
 namespace PizzaGameService.Service.PlayerService.Requests;
 
 public record PlayerRegistrationRequest
 {
-    [JsonPropertyName("login")] public required string Login { get; init; }
+    [JsonPropertyName("login")] 
+    [MinLength(3)]
+    [MaxLength(20)]
+    public required string Login { get; init; }
 
-    [JsonPropertyName("password")] public required string Password { get; init; }
+    [JsonPropertyName("password")] 
+    [MinLength(7)]
+    [MaxLength(20)]
+    [RegularExpression(@"\w*\d\w*")]
+    public required string Password { get; init; }
 
-    [JsonPropertyName("email")] public required string Email { get; init; }
+    [JsonPropertyName("email")] 
+    [EmailAddress]
+    public required string Email { get; init; }
 
-    [JsonPropertyName("age")] public int? Age { get; init; }
+    [JsonPropertyName("age")] 
+    [Range(0,120)]
+    public int? Age { get; init; }
 
-    [JsonPropertyName("gender")] public Gender? Gender { get; init; }
+    [JsonPropertyName("gender")]
+    [Range(0,1)]
+    public Gender? Gender { get; init; }
 }
