c3814a10c9d5cc65b9d62164e55e41f374f484ce
Тимофей <bobylev_tv@taximaxim.ru>
2023-12-16 00:01:52 +0500

    Правки

--- PizzaGameService/src/PizzaGameService.Service/PlayerService/Requests/PlayerRegistrationRequest.cs
+++ PizzaGameService/src/PizzaGameService.Service/PlayerService/Requests/PlayerRegistrationRequest.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
+using PizzaGameService.Data.Player.Models;
 using PizzaGameService.Data.PlayerData.Models;
 
 namespace PizzaGameService.Service.PlayerService.Requests;
